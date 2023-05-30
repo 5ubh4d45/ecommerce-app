02e3e7a6619bfebeccc93b289ff3a18e8637d607
Subha Das <65977621+5ubh4d45@users.noreply.github.com>
2023-05-31 04:32:24 +0530

    init project and setup vue routes

---
+++ ecommerce-client/src/views/AboutPage.vue
@@ -0,0 +1,24 @@
+<!-- create a basic about component in vue 3 -->
+<template>
+  <div>
+    <h1>About</h1>
+    <p>This is the about page</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+/* basic style */
+h1 {
+  font-size: 2rem;
+  font-weight: 700;
+  margin-bottom: 1rem;
+}
+
+p {
+  font-size: 1.25rem;
+  line-height: 1.5;
+}
+</style>
---
+++ ecommerce-client/src/views/HomePage.vue
@@ -0,0 +1,24 @@
+<!-- create a basic Home component in vue 3 -->
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>This is the Home page</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+/* basic style */
+h1 {
+  font-size: 2rem;
+  font-weight: 700;
+  margin-bottom: 1rem;
+}
+
+p {
+  font-size: 1.25rem;
+  line-height: 1.5;
+}
+</style>
