5783a30c6449c4126506a9464bcf5718f5e5f921
Subha Das <connect.subhadas@gmail.com>
2023-07-22 03:42:34 +0530

    init and update spring to latest

--- ecommerce-service/src/main/java/dev/ixale/ecommerceservice/Application.java
+++ ecommerce-service/src/main/java/dev/ixale/ecommerceservice/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceServiceApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(EcommerceServiceApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
